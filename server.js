8573cee0e2c3f7c984e145ce4d98f477197ae83e
Teboho <leroibakiteboho@gmail.com>
2025-02-02 12:56:29 +0200

    mongoDB connected successfully

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 //* IMPORTING REQUIRED MODULES
+require("dotenv").config();
 const express = require("express");
 const app = express();
 path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const mongoose = require("mongoose");
+const connectBD = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
+
+// console.log(process.env.NODE_ENV);
+
+connectBD(); // - Connect to MongoDB
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -33,4 +40,16 @@ app.all("*", (req, res) => {
 
 //* | ERROR HANDLER MIDDLEWARE
 app.use(errorHandler);
-app.listen(PORT, () => console.log(`Server running on port ${PORT}...`)); // - Start the server
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoDB...");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+}); // - Connect to MongoDB and start the server
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoDBLog.log"
+  );
+}); // - Log MongoDB connection errors
